96d445516cd7e276f08d5062130e9d593aa16605
maurocardia <maurocardia27@gmail.com>
2022-12-09 21:34:58 -0500

    inicia deploy

---
+++ client/src/components/DetailCharacter.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import { FaHeart } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "../styles/detailCharacter.css";
+import getConfig from "../utils/getConfig";
+
+const DetailCharacter = () => {
+  const [character, setCharacter] = useState([]);
+  const { id } = useParams();
+
+  const [selectFavorite, setSelectFavorite] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((res) => setCharacter(res.data));
+
+      axios
+      .get(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((res) => setCharacter(res.data));
+
+    axios
+      .get(`${process.env.REACT_APP_HOST}/personajes/${id}`, getConfig())
+      .then((res) => setSelectFavorite(res.data.data?.favorite));
+
+    const idCharacter = {
+      id: id,
+    };
+    axios.post(`${process.env.REACT_APP_HOST}/personajes`, idCharacter, getConfig());
+  }, [selectFavorite]);
+
+  console.log(selectFavorite);
+  const buttonStatus = (status) => {
+    if (status === "Alive") {
+      return <div className="statusAlived"></div>;
+    } else if (status === "Dead") {
+      return <div className="statusDead"></div>;
+    } else {
+      return <div className="statusUnknown"></div>;
+    }
+  };
+
+  const setFavorite = () => {
+    axios
+      .post(`${process.env.REACT_APP_HOST}/personajes/favoritos/${id}`, {}, getConfig())
+      .catch((error) => console.error(error));
+    axios
+      .get(`${process.env.REACT_APP_HOST}/personajes/${id}`, getConfig())
+      .then((res) => setSelectFavorite(res.data.data.favorite));
+  };
+
+  const getFavorite = () => {};
+
+  return (
+    <div className="detailCharacterContainer">
+      <div className="detailCharacter">
+        <div className="namContainer">
+          <h4>{character.name}</h4>
+        </div>
+        <img src={character.image} alt="" />
+        <div className="containerStatus">
+          <div>
+            <h5 className="textStatus">{character.status}</h5>
+          </div>
+          {buttonStatus(character.status)}
+        </div>
+        <h5>
+          Location: <span>{character.location?.name}</span>
+        </h5>
+        <h5>
+          Gender: <span>{character.gender}</span>
+        </h5>
+        <h5>
+          Species: <span>{character.species}</span>
+        </h5>
+        <h5>
+          Type: <span>{character.type}</span>
+        </h5>
+        <br />
+        <div
+          className={selectFavorite ? "favoriteButtonOn" : "favoriteButton"}
+          onClick={() => setFavorite()}
+        >
+          <span> Add to favorites </span>
+          <h1 className="on">
+            <FaHeart />
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailCharacter;
---
+++ client/src/components/User.js
@@ -0,0 +1,259 @@
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import "../styles/user.css";
+import Form from "react-bootstrap/Form";
+import { FaHeart } from "react-icons/fa";
+import { useNavigate, useParams } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import {get, useForm} from "react-hook-form"
+import getConfig from "../utils/getConfig"
+import { URL_API } from "../http/const";
+
+
+const User = () => {
+  const {handleSubmit } = useForm()
+
+  const { id } = useParams();
+  const [characters, setCharacters] = useState([]);
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const [favoriteList,setFavoriteList]=useState()
+  //const [arrayFavorites,setArrayFavorites]=useState()
+  let array=[]
+  const [as,setAs]=useState()
+
+  
+  const [name,setName]=useState("")
+  const [birthdate,setBirthdate]=useState("")
+  const [adress,setAdress]=useState("")
+  const [city,setCity]=useState("")
+
+  const handleClose = () => setShow(false);
+
+  const handleShow = (user) =>{
+  setName(user.name)
+setBirthdate(user.birthdate)
+setAdress(user.adress)
+setCity(user.city)  
+   setShow(true);
+
+  } 
+
+  useEffect(() => {
+    axios
+    .get(`${process.env.REACT_APP_HOST}/personajes`,getConfig())
+    .then((res) => setFavoriteList(res.data.data))
+    .catch(error=>console.error(error))
+  
+    
+
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?page=2`)
+      .then((res) => setCharacters(res.data.results));
+
+    axios
+      .get(`${process.env.REACT_APP_HOST}/usuarios/${id}`,getConfig())
+      .then((res) => setUser(res.data.data.user));
+  }, []);
+
+  const buttonStatus = (status) => {
+    if (status === "Alive") {
+      return <div className="statusAlived"></div>;
+    } else if (status === "Dead") {
+      return <div className="statusDead"></div>;
+    } else {
+      return <div className="statusUnknown"></div>;
+    }
+  };
+
+  const userEdit ={
+      name:name,
+      city,
+      birthdate,
+      adress
+  }
+
+
+  const onSubmit=(data)=>{
+
+    axios
+    .patch(`${process.env.REACT_APP_HOST}/usuarios/${id}`,userEdit, getConfig())
+    .catch(error =>{ console.error(error)})
+    axios
+      .get(`${process.env.REACT_APP_HOST}/usuarios/${id}`,getConfig())
+      .then((res) => setUser(res.data.data.user));
+    alert("se ha modificado el usuario con exito!")
+    handleClose()
+
+    
+    
+    
+
+  }
+
+  const arrayCharacteres=()=>{
+    favoriteList?.map((favorite)=>{
+      const character = favorite.ref_api
+      array.push(character)
+    })
+ }
+
+
+
+ const toFavorites=(e)=>{
+  arrayCharacteres()
+  const list = array.toString()
+  console.log(list)
+  axios.get(`https://rickandmortyapi.com/api/character/${list}`)
+  .then(res=>{setAs(res.data)
+              setCharacters(as)
+              console.log(characters)})
+axios.get(`https://rickandmortyapi.com/api/character/${list}`)
+.then(res=>{setCharacters(res.data)})
+
+
+ }
+
+ const refreshList = ()=>{
+  axios
+  .get(`https://rickandmortyapi.com/api/character/?page=2`)
+  .then((res) => setCharacters(res.data.results));
+ }
+
+
+console.log(characters)
+  return (
+    <div className="containerCharacterPage">
+      <div className="containerUser">
+        <div className="containerBanner">
+          <div className="banner"></div>
+        </div>
+        <div className="secondBar">
+          <div className="user">
+            <div className="userIcon">
+              <AiOutlineUser />
+            </div>
+            <h5 className="userInSession">{user?.name}</h5>
+          </div>
+
+          <div className="controlBar">
+           <button className="selectPersonaje" onClick={toFavorites}>Favoritos</button>
+          </div>
+          <div className="controlBar">
+           <button className="selectPersonaje" onClick={refreshList}>Todos </button>
+          </div>
+          <div className="editUserForm">
+            <Button className="buttonEdit" variant="primary" onClick={handleShow}>
+              Edit User
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Editar informacion</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder={user?.name}
+                      autoFocus
+                     value={name}
+                     onChange={e=>setName(e.target.value)}
+                     
+
+                     
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput2"
+                  >
+                    <Form.Label>Adress</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder={(!user.adress)?"CARRERA 50 # 3-28":user.adress}
+                      autoFocus
+                      value={adress}
+                      onChange={e=>setAdress(e.target.value)}
+                      
+                    
+                    />
+                  </Form.Group>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput3"
+                  >
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder={(!user.city)?"Cali":user.city}
+                      autoFocus
+                      value={city}
+                      onChange={e=>setCity(e.target.value)}
+                    
+                      
+                    />
+                    <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlInput1"
+                  >
+                    <Form.Label>Birthdate</Form.Label>
+                    <Form.Control
+                      type="date"
+                      placeholder={(!user.birthdate)?"YYYY-MM-DD":user.birthdate}
+                      autoFocus
+                      value={birthdate}
+                      onChange={e=>setBirthdate(e.target.value)}
+                     
+                     
+                    />
+                  </Form.Group>
+                    <br />
+                  <Button variant="primary" type="submit" onClick={onSubmit}>
+                  Save Changes
+                </Button>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        </div>
+      </div>
+      <div className="containerCharacter">
+        {characters?.map((character) => (
+          <div className="character" key={character.id}>
+            <img
+              src={character.image}
+              alt=""
+              onClick={() => navigate(`/user/${character.id}`)}
+            />
+            <h5>{character.name}</h5>
+            <div className="containerStatus">
+              <div>
+                <p>{character.status}</p>
+              </div>
+              {buttonStatus(character.status)}
+           
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default User;
